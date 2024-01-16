6d81db2eea73b3fd43bf0b0c0eb3b137f50fb1fc
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-16 16:46:18 +0100

    used ref to read the button click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
+  const PlayerName = useRef();
 
-  const[enteredPlayerName, setEnteredPlayerName] = useState('')
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setEnteredPlayerName(PlayerName.current.value);
+  };
 
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown user'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+        type="text" 
+        ref={PlayerName} 
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
