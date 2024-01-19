e9208078e6aabcf6c3e2a5bf36f7c7611673394b
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-19 17:12:05 +0100

    changed to work with div instead of dialog

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,19 +1,18 @@
-import { forwardRef, useInperativeHandle, useRef } from "react";
-  
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef (function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
   const dialog = useRef();
 
-  useInperativeHandle(ref, () => {
-    return{
+  useImperativeHandle(ref, () => {
+    return {
       open() {
-
-      }
-    }
+        // dialog.current.showModal();
+      },
+    };
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <div ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time <strong>{targetTime} seconds.</strong>
@@ -24,8 +23,8 @@ const ResultModal = forwardRef (function ResultModal({result, targetTime}, ref) 
       <form method="dialog">
         <button>Close</button>
       </form>
-    </dialog>
+    </div>
   );
-})
+});
 
 export default ResultModal;
