8dbbe8a201b055aafcb5bb5e481089750bacb230
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-19 17:38:04 +0100

    added more functionality to app like ability to reset the function

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,19 +5,22 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const[timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
   const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  if(timeRemaining <= 0){
+  if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000)
     dialog.current.open();
   }
 
+  const handleReset = () => {
+    setTimeRemaining(targetTime * 1000);
+  };
+
   const handleStart = () => {
     timer.current = setInterval(() => {
-      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
     }, 10);
   };
 
@@ -27,7 +30,7 @@ export default function TimerChallenge({ title, targetTime }) {
   };
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
